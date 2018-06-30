5e3ef4e2621084538ae377a91728405ca480b915
Gabriel Adewumi <codergab@gmail.com>
2018-06-30 21:05:48 +0100

    Fixing Service Worker Online

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-// Check if API is not reachable
+const currencyStore = idb.open('currency_rates', 4, upgradeDB => {
+	upgradeDB.createObjectStore('converted_rates', {hello: 'World'});
+})
 
+converted_rates.add('rates', {'hello':'world'});
+
+// currencyStore.add('USD_PHP', {conversion_value: 34.54}).then((resolve,reject) => {
+// 	resolve(console.log('Added'));
+// 	reject(console.log('Error'))
+// })
+// currencyStore.set('foo', {hello: 'world'});
 // Free Currency API
 let convertBtn = document.querySelector('#convertBtn');
 let optionVals1 = document.querySelector('.currency');
@@ -48,40 +57,35 @@ let handleConversion = () => {
 	let option2 = document.querySelector('.currency2').value;
 	let conversionValue = document.querySelector('#value').value;
 
-	// if(typeof conversionValue != "number") {
-		
-	// 	statusDiv.innerHTML = "<div class='notification is-warning'>Conversion Number must be a number</div>";
-	// 	resetBtn();
-	// }else {
-		let query =  `${option1}_${option2}`;
-		let conversionUrl = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
-		console.log(conversionUrl);
-		// Send Conversion Request
-		fetch(conversionUrl)
-			.then(res => res.json())
-			.then( converted => {
-				statusDiv.innerHTML = "<div class='notification is-success'>Converted </div>";
-				console.log(converted);
-				const outputHtml = document.getElementById('convertedView');
-				for(let conv in converted) {
-					let conversionRate = converted[conv].val;
-					let convertedValue = conversionValue * conversionRate;
-					// console.log(convertedValue);
-					// alert(`${conversionValue} ${option1} to ${option2} = ${convertedValue}`);
-					statusDiv.innerHTML = `<div class='notification is-info'>
-					${conversionValue} ${option1} to ${option2} = ${Math.floor(convertedValue *100) / 100}</div>`;
-					resetBtn();
-				}
-			})
-			.catch((err) => {
-				console.error(err);
-				
-				statusDiv.innerHTML = `<div class='notification is-danger'>
-				<button class="delete"></button>
-				Ooops!, Something Went Wrong, Please Try again later...
-				</div>`;
+	let query =  `${option1}_${option2}`;
+	let conversionUrl = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
+	console.log(conversionUrl);
+	// Send Conversion Request
+	fetch(conversionUrl)
+		.then(res => res.json())
+		.then( converted => {
+			statusDiv.innerHTML = "<div class='notification is-success'>Converted </div>";
+			console.log(converted);
+			const outputHtml = document.getElementById('convertedView');
+			for(let conv in converted) {
+				let conversionRate = converted[conv].val;
+				let convertedValue = conversionValue * conversionRate;
+				// console.log(convertedValue);
+				// alert(`${conversionValue} ${option1} to ${option2} = ${convertedValue}`);
+				statusDiv.innerHTML = `<div class='notification is-info'>
+				${conversionValue} ${option1} to ${option2} = ${Math.floor(convertedValue *100) / 100}</div>`;
 				resetBtn();
-			})
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+			
+			statusDiv.innerHTML = `<div class='notification is-danger'>
+			<button class="delete"></button>
+			Ooops!, Something Went Wrong, Please Try again later...
+			</div>`;
+			resetBtn();
+		})
 	// 
 	
 
